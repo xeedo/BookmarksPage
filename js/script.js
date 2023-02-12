05e4becac1e79d25face3aade56eef160330f6e5
Przemysław Grądzik <pggradzik@gmail.com>
2023-02-12 18:20:03 +0100

    Update script
    Links now show when a category was clicked.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,93 @@
 let navList = document.getElementById("nav")
 let linkList = document.getElementById("linkList")
-let navItem
-let linkItem
-let descItem
 
-const navItemList = []
-const linkItemList = []
+const navItemList = [
+	{
+	text: "Category no. 1", 
+	ev: "filterLinks('Category no. 1')"
+	}, 
+	
+	{
+	text: "Category no. 2", 
+	ev: "filterLinks('Category no. 2')"
+	}
+]
 
-navItemList.push({text: "Category no.1", link: "link_1.html"})
-navItemList.push({text: "Category no.2", link: "link_2.html"})
 
-linkItemList.push({text: "Link no. 1", link: "link_1.html", desc: "Short description of link no. 1"})
-linkItemList.push({text: "Link no. 2", link: "link_2.html", desc: "Short description of link no. 2"})
-linkItemList.push({text: "Link no. 3", link: "link_3.html", desc: "Short description of link no. 3"})
-linkItemList.push({text: "Link no. 4", link: "link_4.html", desc: "Short description of link no. 4"})
+const linkItemList = [
+	{
+	cat: navItemList[0].text, 
+	text: "Link no. 1", 
+    link: "", 
+	desc: "Short description of link no. 1"
+	}, 
+	
+	{
+	cat: navItemList[0].text, 
+	text: "Link no. 1", 
+    link: "", 
+	desc: "Short description of link no. 2"
+	}, 
+	
+	{
+	cat: navItemList[1].text, 
+	text: "Link no. 3", 
+    link: "", 
+	desc: "Short description of link no. 3"
+	}, 
+	
+	{
+	cat: navItemList[1].text, 
+	text: "Link no. 4", 
+    link: "", 
+	desc: "Short description of link no. 4"
+	} 
+]
 
 for (item of navItemList) {
     addLinks(item, navList)
 }
 
-for (item of linkItemList) {
-    addLinks(item, linkList)
-}
 
 function addLinks(element, nList) {
-    navItem = document.createElement("li")
-    linkItem = document.createElement("a")
-
+	let navItem = document.createElement("li")
+	
+	let linkItem = document.createElement("a")
     linkItem.innerHTML = element.text
-    linkItem.href = element.link
+	
+	if (element.hasOwnProperty("href")) {
+		linkItem.href = element.link
+	}
     navItem.appendChild(linkItem)
 
     if (element.hasOwnProperty("desc")) {
-        descItem = document.createElement("p")
+		let descItem = document.createElement("p")
+		
         descItem.innerHTML = element.desc
         navItem.appendChild(descItem)
     }
+	
+	if (element.hasOwnProperty("ev")) {
+		navItem.setAttribute('onclick', element.ev)
+	}
     
     nList.appendChild(navItem)
+}
+
+function filterLinks(category) {
+	const itemList = []
+	
+	while (linkList.hasChildNodes()) {
+		linkList.removeChild(linkList.firstChild)
+	}
+	
+	for (item of linkItemList) {
+		if (item.cat === category) {
+			itemList.push(item)
+		}
+	}
+	
+	for (item of itemList) {
+		addLinks(item, linkList)
+	}
 }
